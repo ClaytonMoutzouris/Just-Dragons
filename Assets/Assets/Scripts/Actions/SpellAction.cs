5376cf4b1bde71e76f807521b0e5a7cb7df36981
ClaytonMoutzouris <clayton_moutzouris@hotmail.com>
2018-04-03 23:54:29 -0400

    Updating the spell system
    TODO: Add the spell database
    Decide if spells are a subclass of actions, or if there is a spellaction
    type that invokes them

---
+++ Assets/Assets/Scripts/Actions/SpellAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SpellAction : Action
+{
+    [SerializeField]
+    public Spell spell;
+
+
+    public override void Use(Entity user)
+    {
+        spell.Select(user);
+
+    }
+
+}
---
+++ Assets/Assets/Scripts/Actions/Spells/DamageSpell.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSpell : Spell
+{
+    [SerializeField]
+    SpellTargeting targeting;
+    [SerializeField]
+    Vector2 DamageRange;
+    [SerializeField]
+    int range;
+
+    public override bool Cast(Entity caster, List<Entity> Targets)
+    {
+
+        switch (targeting)
+        {
+            case SpellTargeting.Single:
+                Targets[0].GetComponent<Health>().TakeDamage((int)Random.Range(DamageRange.x, DamageRange.y));
+                break;
+            case SpellTargeting.Area:
+
+                break;
+        }
+        caster.GetComponent<ITurnHandler>().SetTurnState(TurnState.End);
+        Deselect(caster);
+        return true;
+    }
+
+    public override void Select(Entity caster)
+    {
+        //Check to see if the requirements are met
+        caster.GetComponent<PlayerTurnHandler>().spellToConfirm = this;
+    }
+
+    public override void Deselect(Entity caster)
+    {
+        //Check to see if the requirements are met
+        caster.GetComponent<PlayerTurnHandler>().spellToConfirm = null;
+    }
+}
